64bcccbd379d2f42d71f431e73182f37a35f96d1
James Pettit <etherealmachine@gmail.com>
2017-02-01 13:42:29 -0800

    Merging changes from Leeps-Lab/otree-core.

--- _static/webcomponents/otree-continuous-bimatrix/heatmap.js
+++ _static/webcomponents/otree-continuous-bimatrix/heatmap.js
@@ -45,6 +45,30 @@ function make_heatmap(canvas_id, payoffs) {
     ctx.putImageData(imageData, 0, 0);
 }
 
+function make_thermometer(canvas_id) {
+    var canvas = document.getElementById(canvas_id);
+    var w = canvas.width;
+    var h = canvas.height;
+    var ctx = canvas.getContext('2d');
+
+    // create empty imageData object
+    var imageData = ctx.createImageData(w, h);
+    var data = imageData.data;
+
+    for (var row = 0; row < h; row++) {
+        point_color = get_gradient_color(1 - (row / h));
+        for (var col = 0; col < w; col++) {
+            data[(row * w * 4) + (col * 4)] = point_color[0];
+            data[(row * w * 4) + (col * 4) + 1] = point_color[1];
+            data[(row * w * 4) + (col * 4) + 2] = point_color[2];
+            // set alpha channel to fully opaque
+            data[(row * w * 4) + (col * 4) + 3] = 255;
+        }
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+}
+
 // weathermap
 // var color_stops = [
 // 	[255, 0, 0],
