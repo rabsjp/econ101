929f0aa1e5912923e04d39cfa2d7a0fda26fccc8
KellerJordan <kellerjordan1998@gmail.com>
2017-03-10 16:35:41 -0800

    fix continuous bimatrix data input

--- continuous_bimatrix/views.py
+++ continuous_bimatrix/views.py
@@ -12,33 +12,31 @@ import json
 import logging
 
 def vars_for_all_templates(self):
-    payoff_grid = None
-    payoff_grid = [
-        [None, None], [None, None],
-        [None, None], [None, None]
-    ]
+    payoff_grid = Constants.payoff_grid
     if (self.player.id_in_group == 1):
-        payoff_grid[0][0] = Constants.p1_A_p2_A_amount
-        payoff_grid[0][0] = Constants.p1_A_p2_B_amount
-        payoff_grid[1][0] = Constants.p1_B_p2_A_amount
-        payoff_grid[1][0] = Constants.p1_B_p2_B_amount
-        payoff_grid[0][1] = Constants.p2_A_p1_A_amount
-        payoff_grid[0][1] = Constants.p2_A_p1_B_amount
-        payoff_grid[1][1] = Constants.p2_B_p1_A_amount
-        payoff_grid[1][1] = Constants.p2_B_p1_B_amount
+        return {
+            "my_A_A_payoff": payoff_grid[0][0],
+            "my_A_B_payoff": payoff_grid[1][0],
+            "my_B_A_payoff": payoff_grid[2][0],
+            "my_B_B_payoff": payoff_grid[3][0],
+            "other_A_A_payoff": payoff_grid[0][1],
+            "other_A_B_payoff": payoff_grid[1][1],
+            "other_B_A_payoff": payoff_grid[2][1],
+            "other_B_B_payoff": payoff_grid[3][1],
+            "total_q": 1
+        }
     else:
-        payoff_grid[0][0] = Constants.p2_A_p1_A_amount,
-        payoff_grid[0][0] = Constants.p2_A_p1_B_amount,
-        payoff_grid[1][0] = Constants.p2_B_p1_A_amount,
-        payoff_grid[1][0] = Constants.p2_B_p1_B_amount,
-        payoff_grid[0][1] = Constants.p1_A_p2_A_amount,
-        payoff_grid[0][1] = Constants.p1_A_p2_B_amount,
-        payoff_grid[1][1] = Constants.p1_B_p2_A_amount,
-        payoff_grid[1][1] = Constants.p1_B_p2_B_amount,
-    return {
-        'payoff_grid': json.dumps(payoff_grid),
-        'total_q': 1
-    }
+        return {
+            "my_A_A_payoff": payoff_grid[0][1],
+            "my_A_B_payoff": payoff_grid[1][1],
+            "my_B_A_payoff": payoff_grid[2][1],
+            "my_B_B_payoff": payoff_grid[3][1],
+            "other_A_A_payoff": payoff_grid[0][0],
+            "other_A_B_payoff": payoff_grid[1][0],
+            "other_B_A_payoff": payoff_grid[2][0],
+            "other_B_B_payoff": payoff_grid[3][0],
+            "total_q": 1
+        }
 
 
 class Introduction(Page):
