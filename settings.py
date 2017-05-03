545041fd410a84b6cc8add1c98d45162926c200e
Leeps <leepslab@gmail.com>
2017-05-03 15:12:02 -0700

    Fixing up stochastic to use the new redwood event emitter.

--- settings.py
+++ settings.py
@@ -166,12 +166,12 @@ SESSION_CONFIGS = [
     #    'num_demo_participants': 2,
     #    'app_sequence': ['discrete_time_example', 'payment_info'],
     #},
-    #{
-    #    'name': 'stochastic_bimatrix',
-    #    'display_name': "Stochastic Bimatrix Game",
-    #    'num_demo_participants': 2,
-    #    'app_sequence': ['stochastic_bimatrix', 'payment_info'],
-    #},
+    {
+        'name': 'stochastic_bimatrix',
+        'display_name': "Stochastic Bimatrix Game",
+        'num_demo_participants': 2,
+        'app_sequence': ['stochastic_bimatrix', 'payment_info'],
+    },
     {
         'name': 'public_goods',
         'display_name': "Public Goods",
