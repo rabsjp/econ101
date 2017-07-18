3bc265eafecd1b519d44459864cf8668dd2c2478
LEEPS Lab <leepslab@gmail.com>
2017-07-18 15:21:48 -0700

    Added otree-continuous-decision-bot for load testing.

--- settings.py
+++ settings.py
@@ -16,6 +16,11 @@ if environ.get('OTREE_PRODUCTION') not in {None, '', '0'}:
 else:
     DEBUG = True
 
+INTERNAL_IPS = (
+    '0.0.0.0',
+    '127.0.0.1',
+)
+
 # don't share this with anybody.
 SECRET_KEY = '{{ secret_key }}'
 
