5fac8f8b74d012caf5fa2f272c6f05427c673b4c
LEEPS Lab <leepslab@gmail.com>
2017-08-10 12:16:48 -0700

    Incorrect function call in stochastic bimatrix. Fiddling with server process for lower latency.

--- runserver.sh
+++ runserver.sh
@@ -1,11 +1,7 @@
 daphne -b 0.0.0.0 -p 8000 otree.asgi:channel_layer &
-python manage.py runworker &
-python manage.py runworker &
-python manage.py runworker &
-python manage.py runworker &
-python manage.py runworker &
-python manage.py runworker &
-python manage.py runworker &
-python manage.py runworker &
-python manage.py runworker &
-python manage.py runworker
+python manage.py runworker --only-channels=http.* --only-channels=otree.* &
+python manage.py runworker --only-channels=http.* --only-channels=otree.* &
+python manage.py runworker --only-channels=websocket.* &
+python manage.py runworker --only-channels=websocket.* &
+python manage.py runworker --only-channels=websocket.* &
+python manage.py runworker --only-channels=websocket.*
