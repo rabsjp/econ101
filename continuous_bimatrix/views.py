1f785c2f2fed65c61097279c49a2a89890ffc038
James Pettit <etherealmachine@gmail.com>
2017-08-22 19:35:46 -0700

    More useful tick generation for continuous and stochastic.

--- continuous_bimatrix/views.py
+++ continuous_bimatrix/views.py
@@ -3,7 +3,8 @@ from __future__ import division
 from ._builtin import Page, WaitPage
 from .models import Constants, Player
 
-import datetime
+from datetime import timedelta
+
 
 class Introduction(Page):
     timeout_seconds = 100
@@ -88,48 +89,56 @@ class Results(Page):
 
 def get_output_table(events):
     header = [
+        'session_code',
+        'subsession_id',
+        'id_in_subsession',
         'tick',
-        'player1',
-        'player2',
+        'p1_mean_strategy',
+        'p2_mean_strategy',
+        'p1_code',
+        'p2_code',
     ]
+    if not events:
+        return [], []
     rows = []
     minT = min(e.timestamp for e in events)
     maxT = max(e.timestamp for e in events)
     last_p1_mean = float('nan')
     last_p2_mean = float('nan')
+    p1, p2 = events[0].group.get_players()
+    p1_code = p1.participant.code
+    p2_code = p2.participant.code
+    group = events[0].group
     for tick in range((maxT - minT).seconds):
-        currT = minT + datetime.timedelta(seconds=tick)
-        tick_events = []
+        currT = minT + timedelta(seconds=tick)
+        group_decisions_events = []
         while events[0].timestamp <= currT:
             e = events.pop(0)
-            if e.channel == 'decisions' and e.value is not None:
-                tick_events.append(e)
+            if e.channel == 'group_decisions':
+                group_decisions_events.append(e)
         p1_decisions = []
         p2_decisions = []
-        for event in tick_events:
-            player = Player.objects.get(
-                participant=event.participant,
-                group=e.group)
-            if player.id_in_group == 1:
-                p1_decisions.append(event.value)
-            elif player.id_in_group == 2:
-                p2_decisions.append(event.value)
-            else:
-                raise ValueError('Invalid player id in group {}'.format(player.id_in_group))
+        for event in group_decisions_events:
+            p1_decisions.append(event.value[p1_code])
+            p2_decisions.append(event.value[p2_code])
         p1_mean, p2_mean = last_p1_mean, last_p2_mean
         if p1_decisions:
             p1_mean = sum(p1_decisions) / len(p1_decisions)
         if p2_decisions:
             p2_mean = sum(p2_decisions) / len(p2_decisions)
         rows.append([
+            group.session.code,
+            group.subsession_id,
+            group.id_in_subsession,
             tick,
             p1_mean,
-            p2_mean
+            p2_mean,
+            p1_code,
+            p2_code,
         ])
         last_p1_mean = p1_mean
         last_p2_mean = p2_mean
     return header, rows
-
 
 page_sequence = [
     Introduction,
